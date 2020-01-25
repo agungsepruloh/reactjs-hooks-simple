d7cf20754684b0bcc1586b2a087ca803741024fb
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-25 17:06:12 +0700

    Code Reuse with Hooks

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,31 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const useResources = resource => {
-  const [resources, setResources] = useState([]);
-
-  const fetchResource = async resource => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-    setResources(response.data);
-  };
-
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource]);
-
-  return resources;
-
-  //   useEffect(() => {
-  //     (async resource => {
-  //       const response = await axios.get(
-  //         `https://jsonplaceholder.typicode.com/${resource}`
-  //       );
-  //       setResources(response.data);
-  //     })(resource);
-  //   }, [resource]);
-};
+import React from "react";
+import useResources from "./useResources";
 
 const ResourceList = ({ resource }) => {
   const resources = useResources(resource);
